da3d6a6693b825201b57ff3a372b4d642ca7c978
janrembold <janrembold@gmail.com>
2021-09-14 11:35:55 +0200

    add esbuild script

---
+++ scripts/esbuild.js
@@ -0,0 +1,32 @@
+const { build } = require('esbuild');
+const path = require('path');
+
+const ES_BUILD_CONFIG = {
+  entryPoints: [
+      path.join('lambda', 'migrate', 'index.ts'),
+      path.join('lambda', 'postgraphile', 'index.ts'),
+  ],
+  external: ['pg-native'],
+  bundle: true,
+  loader: {
+    '.ttf': 'file',
+    '.svg': 'file',
+    '.png': 'dataurl',
+  },
+  minify: true,
+  platform: 'node',
+  outdir: 'lambda/dist',
+  sourcemap: true,
+  target: 'node14',
+  treeShaking: true,
+};
+
+const startBuild = async () => {
+  try {
+    await build(ES_BUILD_CONFIG);
+  } catch (error) {
+    console.error('Build Failed!', error);
+  }
+};
+
+startBuild();
