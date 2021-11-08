9790d8830ee597b0cf1f29eda73230e4ec57fd20
janrembold <janrembold@gmail.com>
2021-11-08 16:23:31 +0100

    add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express, { Application, Request, Response } from "express";
 import cors from 'cors';
-import postgraphile from "postgraphile";
+// import postgraphile from "postgraphile";
 import { migrateUp } from "./migrate/migrate";
 import { getDbConnectionString } from "./utils/getDbConnectionString";
+import { sign } from 'jsonwebtoken';
+import http from 'http';
 
 // Add https://github.com/graphile/worker OR https://github.com/timgit/pg-boss
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
 
 const bootstrap = async () => {
   const connectionString = process.env.DEV_CONNECTION || await getDbConnectionString(process.env.SECRET_ARN);
@@ -18,46 +56,64 @@ const bootstrap = async () => {
 
   const app: Application = express();
   
-  app.use(cors())
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
   
-  app.get("/", async (req: Request, res: Response) => {
+  await server.start();
+    
+  server.applyMiddleware({ app, cors: true });
+  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
+  
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  
+  
+  
+
+
+  // app.use(cors());
+  
+  app.get("/foo", async (req: Request, res: Response) => {
     res.send(`TS App is Running - ${(process.env.SECRET_ARN || 'foobar').slice(0, 10)}`)
   }); 
 
-  app.get("/health", async (_req: Request, res: Response) => {
-    res.status(200).json({
-      uptime: process.uptime(),
-      message: 'Ok',
-      date: new Date()
-    });
-  }); 
+  // app.get("/health", async (_req: Request, res: Response) => {
+  //   res.status(200).json({
+  //     uptime: process.uptime(),
+  //     message: 'Ok',
+  //     date: new Date()
+  //   });
+  // }); 
 
-  app.use(
-      postgraphile(
-          connectionString,
-          "public",
-          {
-              watchPg: true,
-              graphiql: true,
-              enhanceGraphiql: true,
-              // jwtSecret: 'supersecretjwttokenpass',
-              // jwtPgTypeIdentifier: 'public.jwt_token',
-              // pgDefaultRole: 'user_guest'
-          } 
-      )
-  );
+  // app.use(
+  //     postgraphile(
+  //         connectionString,
+  //         "public",
+  //         {
+  //             watchPg: true,
+  //             graphiql: true,
+  //             enhanceGraphiql: true,
+  //             // jwtSecret: 'supersecretjwttokenpass',
+  //             // jwtPgTypeIdentifier: 'public.jwt_token',
+  //             // pgDefaultRole: 'guest'
+  //         } 
+  //     )
+  // );
   
-  const expressPort = process.env.PORT || 8080;
-  const server = app.listen(expressPort, () => {
-      console.log(`Blitz Backend is running on PORT ${expressPort}`)
-  }); 
+  // const expressPort = process.env.PORT || 8080;
+  // const server = app.listen(expressPort, () => {
+  //     console.log(`Blitz Backend is running on PORT ${expressPort}`)
+  // }); 
 
   process.on('SIGTERM', () => {
     console.log('SIGTERM signal received: closing Express server')
 
-    server.close(() => {
-      console.log('Express server successfully closed')
-    })
+    // server.close(() => {
+    //   console.log('Express server successfully closed')
+    // })
   })
 }
 
--- src/migrate/db.sql
+++ src/migrate/db.sql
@@ -1,6 +1,13 @@
+-- https://medium.com/swlh/postgres-role-inheritance-and-policies-for-beginners-4ae13f973bb1
+
 -- Install extensions
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE EXTENSION postgis;
+CREATE EXTENSION pgrouting;
+
+https://api.openstreetmap.org/api/0.6/map?bbox=7.79153,48.15994,7.7988,48.16444
 
 -- Create schemas
 DROP SCHEMA IF EXISTS private_schema CASCADE;
@@ -16,6 +23,32 @@ CREATE TYPE public.jwt_token as (
 );
 
 
+-- Create roles
+CREATE ROLE base_role NOINHERIT;
+CREATE ROLE administrator INHERIT;
+CREATE ROLE player INHERIT;
+CREATE ROLE guest INHERIT;
+
+GRANT base_role TO administrator, player, guest;
+
+-- CREATE ROLE user_login;
+-- CREATE ROLE user_guest;
+-- GRANT administrator to postgres;
+-- GRANT user_login to postgres, user_admin;
+-- GRANT user_guest to postgres, user_login;
+
+
+
+-- Grant usage
+GRANT USAGE ON SCHEMA public to base_role;
+GRANT USAGE ON SCHEMA private_schema to administrator;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA private_schema, public TO administrator;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO player;
+
+
+
 -- Create tables
 CREATE TABLE public.users
 (
@@ -29,6 +62,7 @@ CREATE TABLE private_schema.authentications
     id integer PRIMARY KEY,
     email text UNIQUE NOT NULL,
     password text NOT NULL,
+    role text NOT NULL,
     user_id integer NOT NULL,
 
     CONSTRAINT fk_user
@@ -54,7 +88,7 @@ BEGIN
 
   if account.password = crypt(password, account.password) then
     return (
-      'user_login',
+      account.role,
       extract(epoch FROM now() + interval '15 days'),
       extract(epoch FROM interval '15 days'),
       account.user_id
@@ -75,7 +109,7 @@ begin
 
   if FOUND then
     return (
-      'user_login', 
+      account.role, 
       extract(epoch FROM now() + interval '15 days'),
       extract(epoch FROM interval '15 days'),
       account.user_id
@@ -90,46 +124,77 @@ CREATE FUNCTION public.current_user_id() RETURNS integer as $$
   SELECT nullif(current_setting('jwt.claims.user_id', true), '')::integer;
 $$ language sql stable;
 
-
--- Create roles
-DROP ROLE IF EXISTS user_admin;
-CREATE ROLE user_admin;
-GRANT user_admin to postgres;
-
-DROP ROLE IF EXISTS user_login;
-CREATE ROLE user_login;
-GRANT user_login to postgres, user_admin;
-
-DROP ROLE IF EXISTS user_guest;
-CREATE ROLE user_guest;
-GRANT user_guest to postgres, user_login;
-
-
-
--- Grant usage
-GRANT USAGE ON SCHEMA public to user_login;
-GRANT USAGE ON SCHEMA private_schema to user_admin;
-
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO user_admin;
-
-GRANT ALL ON TABLE public.users TO user_login;
-GRANT EXECUTE ON FUNCTION public.authenticate(text, text) to user_guest;
-GRANT EXECUTE ON FUNCTION public.refresh_token() to user_login;
+GRANT EXECUTE ON FUNCTION public.authenticate(text, text) to guest;
+GRANT EXECUTE ON FUNCTION public.refresh_token() to player;
 
 
 
 -- Create policies
-CREATE POLICY select_users ON public.users FOR SELECT TO user_login USING (id = public.current_user_id());
-CREATE POLICY select_authentications ON private_schema.authentications FOR SELECT TO user_admin USING (user_id = public.current_user_id());
-CREATE POLICY select_all_users on public.users FOR ALL TO user_admin USING (true);
+CREATE POLICY select_users ON public.users FOR SELECT TO player USING (id = public.current_user_id());
+CREATE POLICY select_authentications ON private_schema.authentications FOR SELECT TO administrator USING (user_id = public.current_user_id());
+CREATE POLICY select_all_users on public.users FOR ALL TO administrator USING (true);
 
 
 
 -- Insert data
 INSERT INTO public.users (id, name) VALUES (1, 'Admin');
-INSERT INTO public.users (id, name) VALUES (2, 'Foo');
-INSERT INTO public.users (id, name) VALUES (3, 'Bar');
+INSERT INTO private_schema.authentications ( id, user_id, email, password, role) 
+VALUES (10, 1, 'admin@example.com', crypt('pass', gen_salt('bf', 8)), 'administrator');
 
-INSERT INTO private_schema.authentications (id, user_id, email, password) VALUES (1, 1, 'admin@example.com', crypt('pass', gen_salt('bf', 8)));
-INSERT INTO private_schema.authentications (id, user_id, email, password) VALUES (3, 3, 'bar@example.com', crypt('pass', gen_salt('bf', 8)));
-INSERT INTO private_schema.authentications (id, user_id, email, password) VALUES (2, 2, 'foo@example.com', crypt('pass', gen_salt('bf', 8)));
+INSERT INTO public.users (id, name) VALUES (2, 'Foo');
+INSERT INTO private_schema.authentications ( id, user_id, email, password, role) 
+VALUES (20, 2, 'foo@example.com', crypt('pass', gen_salt('bf', 8)), 'player');
+
+INSERT INTO public.users (id, name) VALUES (3, 'Bar');
+INSERT INTO private_schema.authentications ( id, user_id, email, password, role) 
+VALUES (30, 3, 'bar@example.com', crypt('pass', gen_salt('bf', 8)), 'player');
+
+
+CREATE TABLE public.ships
+(
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    x integer,
+    y integer,
+    user_id integer NOT NULL,
+
+    CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+	    REFERENCES public.users(id)
+          ON DELETE CASCADE
+);
+ALTER TABLE public.ships ENABLE ROW LEVEL SECURITY;
+GRANT SELECT ON public.ships TO player;
+CREATE POLICY select_ships ON public.ships FOR SELECT TO player USING (true);
+
+
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (1, 2, 'Ship1.1', 10, 10);
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (2, 2, 'Ship1.2', 30, 10);
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (3, 2, 'Ship1.3', 10, 30);
+
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (4, 3, 'Ship2.1', 50, 50);
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (5, 3, 'Ship2.2', 80, 5);
+INSERT INTO public.ships (id, user_id, name, x, y) VALUES (6, 3, 'Ship2.3', null, null);
+
+
+
+
+
+INSERT INTO edge_table (
+    category_id, reverse_category_id,
+    cost, reverse_cost,
+    x1, y1,
+    x2, y2,
+    dir) VALUES
+(1, 1, 2, 2,  1, 1,  1, 3, 'B'),
+(1, 1, 3, 3,  1, 3,  2, 6, 'B'),
+(1, 1, 6, 6,  2, 6,  5, 2, 'B'),
+(1, 1, 3, 3,  1, 1,  4, 1, 'B'),
+(1, 1, 1, 1,  4, 1,  5, 2, 'B'),
+(1, 1, 1, 1,  1, 1,  2, 2, 'B'),
+(1, 1, 1, 1,  2, 2,  4, 2, 'B'),
+(1, 1, 1, 1,  4, 2,  5, 2, 'B');
+
+UPDATE edge_table SET the_geom = st_makeline(st_point(x1,y1),st_point(x2,y2));  
+
+SELECT pgr_createTopology('edge_table',0.001);
